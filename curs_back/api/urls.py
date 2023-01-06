c6061ec52ba5fea3893f72880a478a7be47dad98
bochkovas22pv192 <bochkovsky2002@gmail.com>
2023-01-06 11:59:20 +0300

    added RGZ

--- curs_back/api/urls.py
+++ curs_back/api/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from .views import CursView, LectionsView, CursViewChange
 
+
 router = DefaultRouter()
 
 router.register(r'curs', CursView, basename='curs')
